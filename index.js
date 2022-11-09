71dd4e395ab990e70ff3f45b4e6b7a945b36a3bc
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-11-09 17:07:22 +0200

    api for one specific service set

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,6 +27,13 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
+        app.get('/services/id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollections.findOne(query);
+            res.send(service);
+        })
+
 
     }
     finally {
