ac0d9a15efb97c781755f759eec7560dc4417b03
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-11-11 02:58:29 +0200

    commenting and final moderation done

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
+// middle wire setup
+
 app.use(cors());
 app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uzyhqeg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// jsonwebtoken verification from client side
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
@@ -31,41 +35,64 @@ function verifyJWT(req, res, next) {
 
 async function run() {
     try {
+        // databases from mongobd
+
         const serviceCollections = client.db('carry-you').collection('services');
         const reviewsCollection = client.db('carry-you').collection('reviews');
+
+        // post method inserted to read data from client side
+
         app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10h' })
             res.send({ token });
         })
+
+        // here api created to read services with limitation apply to 3 data from database
+
         app.get('/limitedServices', async (req, res) => {
             const query = {};
             const cursor = serviceCollections.find(query).sort({ date: -1 });
             const limitedServices = await cursor.limit(3).toArray();
             res.send(limitedServices);
         })
+
+        // post method for receive services data from client side and put into database
+
         app.post('/services', async (req, res) => {
             const service = req.body;
             const result = await serviceCollections.insertOne(service);
             res.send(result);
         })
+
+        // api created for all services data database
+
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollections.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        // api created for specific service data from database
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await serviceCollections.findOne(query);
             res.send(service);
         })
+
+        // post method for receive reviews data from client side and insert into database
+
         app.post('/reviews', async (req, res) => {
             const review = req.body;
             const result = await reviewsCollection.insertOne(review);
             res.send(result);
         })
+
+        // api created for reviews data for specific email from database and jwt token check
+
         app.get('/reviews', verifyJWT, async (req, res) => {
             const decoded = req.decoded;
             if (decoded.email !== req.query.email) {
@@ -79,25 +106,37 @@ async function run() {
             const reviews = await cursor.toArray();
             res.send(reviews);
         })
+
+        // api created for specific review data from database
+
         app.get('/review/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await reviewsCollection.findOne(query);
             res.send(result);
         })
+
+        // api created for all reviews data from database and sort by decending order
+
         app.get('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const query = { service: id };
-            const cursor = reviewsCollection.find(query);
+            const cursor = reviewsCollection.find(query).sort({ date: -1 });
             const reviews = await cursor.toArray();
             res.send(reviews);
         })
+
+        // delemethod called for delete data from database
+
         app.delete('/review/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await reviewsCollection.deleteOne(query);
             res.send(result);
         })
+
+        // put method for updating specific review field from a review in database
+
         app.put('/review/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) }
